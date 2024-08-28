5400951603192f61dd3377806062e0f865095ea5
victorhinojosa <victorhinojosa@protonmail.com>
2024-08-27 22:35:22 -0600

    first commit

---
+++ src/main/java/com/example/mindkeep/MindkeepApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindkeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindkeepApplication.class, args);
+	}
+
+}
